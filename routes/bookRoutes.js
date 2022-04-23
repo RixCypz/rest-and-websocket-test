ea341f9d0c317904b0caa364c7672ca96b907925
Chaiyapruek <6010110084@psu.ac.th>
2022-04-23 19:03:14 +0700

    delete comment

--- routes/bookRoutes.js
+++ routes/bookRoutes.js
@@ -6,13 +6,6 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-// router.get('/querytime1', async (req, res) => {
-//   console.log("query");
-//   let data = await main().catch(console.error);
-//   // console.log(data);
-//   res.json(data);
-// });
-
 router.post("/addBook",bookController.addBook);
 router.get("/findBooks",bookController.findBooks);
 router.delete("/deleteBook",bookController.deleteBooks);
